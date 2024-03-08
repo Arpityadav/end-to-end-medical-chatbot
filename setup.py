93d1f0636b60d28a4065bc39ad8c94e44c3eb68d
Arpit <arpit.yadav1912@gmail.com>
2024-03-08 22:43:54 +0530

    End to end medical chatbot done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Arpit',
+    author_email= 'arpit@fakeemail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
---
+++ store_index.py
@@ -0,0 +1,26 @@
+from src.helper import load_pdf, text_split, download_hugging_face_embeddings
+from pinecone import Pinecone
+from langchain_pinecone import PineconeVectorStore
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
+
+# print(PINECONE_API_KEY)
+# print(PINECONE_API_ENV)
+
+extracted_data = load_pdf("data/")
+text_chunks = text_split(extracted_data)
+embeddings = download_hugging_face_embeddings()
+
+
+Pinecone(api_key=os.environ.get('PINECONE_API_KEY'),
+              environment=os.environ.get('PINECONE_API_ENV'))
+
+index_name="medai"
+
+#Creating Embeddings for Each of The Text Chunks & storing
+docsearch=PineconeVectorStore.from_texts([t.page_content for t in text_chunks], embeddings, index_name=index_name)
